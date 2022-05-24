8aba22efcaaeb7d9d3f348c811d0bb3a539d68c8
leracoder2 <lera126141@gmail.com>
2022-05-24 22:06:07 +0500

    Aдаптивная графика - блок живой пример

--- source/js/script.js
+++ source/js/script.js
@@ -41,120 +41,39 @@ navToggle.addEventListener('click', function () {
 
 
 
-
-
-
-
 // IMAGE-CONTROLE
 
-// let imgControlScrollbar = document.querySelector('.image-control__scrollbar');
-// let imgBefore = document.querySelector('.demo__image-before');
-// let imgAfter = document.querySelector('.demo__image-after');
+let imgControlScrollbar = document.querySelector('.image-control__scrollbar');
+let imgBefore = document.querySelector('.demo__image-before');
+let imgAfter = document.querySelector('.demo__image-after');
 
-// imgControlScrollbar.addEventListener('click', function () {
-//   if (imgBefore.classList.contains('demo__image--show')) {
-//     imgBefore.classList.removeClass('demo__image--show');
-//     imgAfter.classList.add('demo__image--show');
-//   } else {
-//     imgBefore.classList.addClass('demo__image--show');
-//     imgAfter.classList.adremoveClass('demo__image--show');
-//   }
-// });
+imgControlScrollbar.addEventListener('click', function () {
+  if (imgBefore.classList.contains('demo__image--show')) {
+    imgBefore.classList.removeClass('demo__image--show');
+    imgAfter.classList.add('demo__image--show');
+  } else {
+    imgBefore.classList.addClass('demo__image--show');
+    imgAfter.classList.adremoveClass('demo__image--show');
+  }
+});
+
+
 
 $( document ).ready(function() {
 
   // image-control
 
-  $(".image-control__scrollbar").on("click", function() {
+  // $(".image-control__scrollbar").on("click", function() {
 
-    $(this).toggleClass('image-control__scrollbar--active');
+  //   $(this).toggleClass('image-control__scrollbar--active');
 
-    if ($('.demo__image-before').hasClass('demo__image--show')) {
-      $('.demo__image-before').removeClass('demo__image--show');
-      $('.demo__image-after').addClass('demo__image--show');
-    } else {
-      $('.demo__image-before').addClass('demo__image--show');
-      $('.demo__image-after').removeClass('demo__image--show');
-    }
-  });
-
-
-  // -------------- Слайдер до | после
-
-  function imageComparison(selector) {
-
-    let comparison = $(selector)
-        .addClass('image-comparison')
-        .prepend('<div class="image-comparison__before"></div>')
-        .append('<div>БЫЛО</div>')
-        .append('<button class="image-comparison__slider"></button>')
-        .append('<div>СТАЛО</div>');
-
-    let images = comparison
-        .find('img')
-        .addClass('image-comparison__image')
-        .css('max-width', comparison.width());
-
-    let before = comparison
-        .find('.image-comparison__before')
-        .append(images.eq(0));
-
-    comparison
-        .find('.image-comparison__slider')
-        .on('dragstart', () => false) // отмена станд. drug&drop
-        .on('mousedown', function(e) {
-            let slider = $(this);
-            let doc = $(document).on('mousemove', (e) => {
-                let offset = e.pageX - comparison.position().left;
-                let width = comparison.width();
-
-                // установим границы, чтобы ползунок не выходил
-                if (offset < 0) offset = 0;
-                if (offset > width) offset = width;
-
-                slider.css('left', offset + 'px');
-                before.css('width', offset + 'px');
-            });
-
-            doc.on('mouseup', () => doc.off('mousemove'));
-        });
-};
-
-imageComparison('#image-comparison');
+  //   if ($('.demo__image-before').hasClass('demo__image--show')) {
+  //     $('.demo__image-before').removeClass('demo__image--show');
+  //     $('.demo__image-after').addClass('demo__image--show');
+  //   } else {
+  //     $('.demo__image-before').addClass('demo__image--show');
+  //     $('.demo__image-after').removeClass('demo__image--show');
+  //   }
+  // });
 
 });
-
-
-// ymaps.ready(function () {
-//   var myMap = new ymaps.Map('map', {
-//           center: [55.751574, 37.573856],
-//           zoom: 9
-//       }, {
-//           searchControlProvider: 'yandex#search'
-//       }),
-
-//       // Создаём макет содержимого.
-//       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-//           '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-//       ),
-
-//       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-//           hintContent: 'HTML Academy',
-//       }, {
-//           // Опции.
-//           // Необходимо указать данный тип макета.
-//           iconLayout: 'default#image',
-//           // Своё изображение иконки метки.
-//           iconImageHref: './img/icons/map-pin.svg',
-//           // Размеры метки.
-//           iconImageSize: [30, 42],
-//           // Смещение левого верхнего угла иконки относительно
-//           iconImageOffset: [-5, -38]
-//       })
-
-//   myMap.geoObjects
-//       .add(myPlacemark);
-// });
-
-
-
